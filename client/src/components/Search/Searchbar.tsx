3a0d3d39862e4e0617d4b2f12d0e6ec7f83de62c
sophiecode1105 <sophiecode1105@gmail.com>
2022-02-14 19:18:54 +0900

    Update: Search바

--- client/src/components/Search/Searchbar.tsx
+++ client/src/components/Search/Searchbar.tsx
@@ -8,20 +8,55 @@ const Container = styled.div`
   width: 100%;
   padding-top: 100px;
   max-width: 1200px;
-  border: 1px solid red;
+  position: relative;
+`;
+
+const SearchBox = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
 `;
 
 const SearchInput = styled.input`
+  text-decoration: none;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  /* border-radius: 10px; */
   width: 60%;
-  border: 2px solid green;
-  padding: 10px;
-  border-radius: 30px;
+  padding: 8px;
+  &:focus {
+    outline-color: green;
+    /* border-bottom: 2px solid rgba(0, 0, 0, 0.2); */
+  }
+`;
+const SearchButton = styled.button`
+  /* height: 40px; */
+  cursor: pointer;
+  background-color: #2f6218;
+  border: 0;
+  outline: 0;
+  color: rgb(242, 242, 242, 0.9);
+  font-weight: 500;
+  &:hover {
+    background-color: rgba(47, 98, 24, 0.8);
+  }
+  font-size: 15px;
+  transition: 0.3s;
+  padding: 0 15px;
+  i {
+    color: white;
+    font-size: 18px;
+  }
 `;
 
 const SearchBar = () => {
   return (
     <Container>
-      <SearchInput></SearchInput>
+      <SearchBox>
+        <SearchInput type="text" placeholder="찾고싶은 도서를 검색해보세요"></SearchInput>
+        <SearchButton type="button">
+          <i className="fas fa-search"></i>
+        </SearchButton>
+      </SearchBox>
     </Container>
   );
 };
