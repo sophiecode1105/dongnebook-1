7034963145be7b839a3aeb2295b9df306854ee97
codingbe <descuento@naver.com>
2022-02-14 19:15:52 +0900

    Update: 상태관리 적용 최대화

--- client/src/state.ts
+++ client/src/state.ts
@@ -1,18 +1,13 @@
 import { atom } from "recoil";
 
+export const userState = atom({
+  key: "user",
+  default: {},
+});
+
 export const loginState = atom({
-  key: "isLogin",
-  default: Boolean(localStorage.getItem("isLogin")),
-});
-
-export const userId = atom({
-  key: "userId",
-  default: "",
-});
-
-export const adminState = atom({
-  key: "admin",
-  default: false,
+  key: "login",
+  default: localStorage.getItem("token"),
 });
 
 export const titleStorage = atom({
