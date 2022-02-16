fe01eac1caee9377a09bceff8cc49163ffd2d18b
codingbe <descuento@naver.com>
2022-02-16 19:02:19 +0900

    Update: 리코일 타입 정의 및 채팅방 컴포넌트 초기화

--- client/src/state.ts
+++ client/src/state.ts
@@ -1,21 +1,40 @@
 import { atom } from "recoil";
 
-export const userState = atom({
+type UserState = {
+  id?: number;
+  nickname?: string;
+  img?: string;
+  admin?: boolean;
+  email?: string;
+  locationId?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  locations?: {
+    createdAt: string;
+    address: string;
+    id: number;
+    lat: number;
+    lon: number;
+    updatedAt: string;
+  };
+};
+
+export const userState = atom<UserState>({
   key: "user",
   default: {},
 });
 
-export const loginState = atom({
+export const loginState = atom<string | null>({
   key: "login",
   default: localStorage.getItem("token"),
 });
 
-export const titleStorage = atom({
+export const titleStorage = atom<string>({
   key: "title",
   default: "",
 });
 
-export const contentStorage = atom({
+export const contentStorage = atom<string>({
   key: "content",
   default: "",
 });
@@ -25,7 +44,7 @@ export const imageStorage = atom({
   default: {},
 });
 
-export const searchLocation = atom({
+export const searchLocation = atom<string>({
   key: "location",
   default: "",
 });
@@ -40,17 +59,17 @@ export const currentLocationStorage = atom({
   default: {},
 });
 
-export const currentLatitude = atom({
+export const currentLatitude = atom<number>({
   key: "currentLatitude",
   default: 0,
 });
 
-export const currentLongtitude = atom({
+export const currentLongtitude = atom<number>({
   key: "currentLongtitude",
   default: 0,
 });
 
-export const currentaddress = atom({
+export const currentaddress = atom<string>({
   key: "address",
   default: "",
 });
