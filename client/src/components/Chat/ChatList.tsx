f33086c88878889d3cbba38a4c39b1c94dd404d7
codingbe <descuento@naver.com>
2022-03-02 22:52:37 +0900

    Update: 채팅방 아무도 없을때 화면 구현

--- client/src/components/Chat/ChatList.tsx
+++ client/src/components/Chat/ChatList.tsx
@@ -2,6 +2,7 @@ import { useSetRecoilState } from "recoil";
 import { timeForToday, socket } from "../../api";
 import { chatRoomFrame, chatRoomVisible } from "../../state/state";
 import { ChatListComponentProps, ChatRoomFrameType, chatRooms } from "../../state/typeDefs";
+import found from "../../img/found.jpg";
 
 const ChatList = ({ chatRooms }: ChatListComponentProps) => {
   const setVisible = useSetRecoilState(chatRoomVisible);
@@ -27,37 +28,43 @@ const ChatList = ({ chatRooms }: ChatListComponentProps) => {
     <div className="h-[90vh] pt-20 max-w-md w-full m-auto p-2">
       <h1 className="text-3xl font-bold pb-3 border-b-2 border-[#7F7F7F] mb-3">채팅목록</h1>
       <ul className="h-full overflow-y-scroll">
-        {chatRooms?.map((chatRoom, idx) => {
-          const { count, productId } = chatRoom;
-          const { img, nickname } = chatRoom.users[0].users;
-          const { content, createdAt } = chatRoom.chats[0];
+        {chatRooms.length !== 0 ? (
+          chatRooms?.map((chatRoom, idx) => {
+            const { count, productId } = chatRoom;
+            const { img, nickname } = chatRoom.users[0].users;
+            const { content, createdAt } = chatRoom.chats[0];
 
-          return (
-            <li
-              key={idx}
-              onClick={() => fetchData(productId)}
-              className="flex text-gray-600 bg-slate-50 h-20 cursor-pointer"
-            >
-              <div className="rounded-full flex items-center justify-center p-1">
-                <img src={img} alt={nickname} className="rounded-full w-12 h-12" />
-              </div>
-              <div className="w-full p-2 flex flex-col justify-between">
-                <div className="flex justify-between">
-                  <h1 className="text-lg font-bold ">{nickname}</h1>
-                  <span className="text-sm">{timeForToday(createdAt)}</span>
+            return (
+              <li
+                key={idx}
+                onClick={() => fetchData(productId)}
+                className="flex text-gray-600 bg-slate-50 h-20 cursor-pointer">
+                <div className="rounded-full flex items-center justify-center p-1">
+                  <img src={img} alt={nickname} className="rounded-full w-12 h-12" />
                 </div>
-                <div className="flex justify-between border-b border-gray-200 pb-3">
-                  <p className="text-xs font-semibold">{content}</p>
-                  {count > 0 && (
-                    <span className="w-5 h-5 rounded-full bg-red-400 flex justify-center items-center text-gray-50">
-                      {count}
-                    </span>
-                  )}
+                <div className="w-full p-2 flex flex-col justify-between">
+                  <div className="flex justify-between">
+                    <h1 className="text-lg font-bold ">{nickname}</h1>
+                    <span className="text-sm">{timeForToday(createdAt)}</span>
+                  </div>
+                  <div className="flex justify-between border-b border-gray-200 pb-3">
+                    <p className="text-xs font-semibold">{content}</p>
+                    {count > 0 && (
+                      <span className="w-5 h-5 rounded-full bg-red-400 flex justify-center items-center text-gray-50">
+                        {count}
+                      </span>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          })
+        ) : (
+          <div className="w-full h-full bg-slate-600 flex justify-center items-center bg">
+            <img src={found} alt="robot" className="w-full h-full" />
+            <p className="absolute text-white font-bold text-3xl">아무도 없네 ...</p>
+          </div>
+        )}
       </ul>
     </div>
   );
