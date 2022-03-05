b302c349086e37a2f3296615dbc47c9de4f346e3
sophiecode1105 <sophiecode1105@gmail.com>
2022-03-05 20:43:42 +0900

    Update: 메인페이지 2차

---
+++ client/src/components/Main/MainScreen.tsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import bookImg from "../../img/book.png";
+import { ReactComponent as SecondLogo } from "../../svg/Dongnebook.svg";
+import { Link, useNavigate } from "react-router-dom";
+
+const Wrap = styled.div`
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 1200px;
+  margin: 0 auto;
+  max-width: 1000px;
+  padding: 66px 10px 10px 10px;
+`;
+const ImgBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  height: 55%;
+`;
+const Img = styled.img`
+  width: 600px;
+`;
+const TitleBox = styled.div`
+  display: flex;
+  justify-content: start;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const LogoBox = styled.div`
+  margin-top: 20px;
+  width: 300px;
+`;
+
+const Button = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+  width: 300px;
+  height: 50px;
+  border-radius: 15px;
+  font-weight: bold;
+  background-color: #e4f3e0;
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-left: 30px;
+`;
+
+const MainScreen = () => {
+  return (
+    <Wrap>
+      <Container>
+        <Div>
+          <TitleBox>
+            <Title>우리동네 </Title>
+            <Title>위치 기반 도서 교환 플랫폼</Title>
+            <LogoBox>
+              <SecondLogo></SecondLogo>
+            </LogoBox>
+            <Button to="/around">체험하기</Button>
+          </TitleBox>
+        </Div>
+        <ImgBox>
+          <Img src={bookImg}></Img>
+        </ImgBox>
+      </Container>
+    </Wrap>
+  );
+};
+
+export default MainScreen;
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import AnimationIcon from "../components/Main/AnimationIcon";
 import Footer from "../components/Main/Footer";
 import Screen from "../components/Main/ChattingService";
+import MainScreen from "../components/Main/MainScreen";
 
 const Main = () => {
   return (
     <>
+      <MainScreen />
       <AnimationIcon />
       <Screen />
       <Footer />
